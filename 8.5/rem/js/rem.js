d030f9133974e86e87d080f7833f77e65079cb4e
guozhao <davidkoojohn@163.com>
2019-08-05 15:44:55 +0800

    add 8.5 page

---
+++ 8.5/rem/js/rem.js
@@ -0,0 +1,11 @@
+(function () {
+  var resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize'
+  var recalc = function (){
+    var cWidth = document.documentElement.clientWidth || document.body.clientWidth;
+    if (!cWidth) return;
+    document.documentElement.style.fontSize = 200 * (cWidth / 750) + 'px';
+  };
+  if (!document.addEventListener) return;
+  window.addEventListener(resizeEvt, recalc, false);
+  document.addEventListener('DOMContentLoaded', recalc, false);
+})()
